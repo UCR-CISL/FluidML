1d212027841c2fbfad543186d59e61c10746c9ef
Jinjie Liu <jinjie.liu@usc.edu>
2025-02-14 02:05:50 -0800

    Add numpy and onnx in the setup deps.
    Signed-off-by: Jinjie Liu <jinjie.liu@usc.edu>

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setuptools.setup(
     install_requires=[
         "iree-base-compiler@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=compiler",
         "iree-base-runtime@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=runtime",
+        "numpy",
+        "onnx",
     ],
 )
