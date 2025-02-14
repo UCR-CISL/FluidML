81c1b79078e75885d4956bb1414d818caf1c7ba9
jinjieliu <jinjie.liu@usc.edu>
2025-02-13 23:00:38 -0800

    add setup deps
    Signed-off-by: jinjieliu <jinjie.liu@usc.edu>

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="fluidml",
+    install_requires=[
+        "iree-base-compiler@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=compiler",
+        "iree-base-runtime@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=runtime",
+    ],
+)
