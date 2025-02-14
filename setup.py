197c9b14dac76372ff52c0fd313f7f8bb33d0d77
Jinjie Liu <jinjie.liu@usc.edu>
2025-02-14 11:47:17 -0800

    create package
    Signed-off-by: Jinjie Liu <jinjie.liu@usc.edu>

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
+print(setuptools.find_packages("src"))
 
 setuptools.setup(
     name="fluidml",
+    author="Jinjie Liu",
+    author_email="jinjie.liu@usc.edu",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
     install_requires=[
         "iree-base-compiler@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=compiler",
         "iree-base-runtime@git+https://github.com/UCR-CISL/iree-fluidml.git@fluidml-dev#subdirectory=runtime",
